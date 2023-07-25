1a61c7197382d748f32a82563754b514fab8e3cf
hydroper <matheusdiasdesouzads@gmail.com>
2023-07-25 15:08:57 -0300

    _

--- src/util/src/code_points/mod.rs
+++ src/util/src/code_points/mod.rs
@@ -1,4 +1,5 @@
-//! Work with code points.
+//! Work with Unicode Code Points. This module
+//! includes interfaces for manipulating strings.
 
 mod code_points_reader;
 pub use code_points_reader::CodePointsReader;
