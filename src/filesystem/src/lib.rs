d6f02c9cba1025ef12413759fc15c8bc00c217d0
hydroper <matheusdiasdesouzads@gmail.com>
2023-08-05 17:23:17 -0300

    _

--- src/filesystem/src/lib.rs
+++ src/filesystem/src/lib.rs
@@ -3,7 +3,7 @@ The Rialight file system API.
 */
 
 use rialight_prelude::*;
-use rialight_util::file_paths::argumented as file_paths;
+use rialight_util::file_paths::{Path, PlatformPathVariant};
 
 mod error;
 pub use error::FileError;
